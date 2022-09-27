bdece127fd59c47c959c8ecf33eaab68b45bdf39
Valentin 'Ookamiko' Dewilde <valentin.dewilde@hotmail.be>
2022-09-27 16:36:25 +0200

    [IMP] Challenge 2 - Bonus 1 - Random spawning time

--- Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,13 +10,13 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float minDelay = 3;
+    private float maxDelay = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", Random.Range(minDelay, maxDelay));
     }
 
     // Spawn random ball at random x position at top of play area
@@ -28,6 +28,9 @@ public class SpawnManagerX : MonoBehaviour
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
+
+        // Randomize spawning
+        Invoke("SpawnRandomBall", Random.Range(minDelay, maxDelay));
     }
 
 }
